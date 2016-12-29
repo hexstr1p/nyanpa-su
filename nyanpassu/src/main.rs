96f4d46beac119ec2cdbd3ad447a6bf2026cca48
panpanick <claud@openmailbox.org>
2016-12-29 03:26:37 -0500

    .

--- nyanpassu/src/main.rs
+++ nyanpassu/src/main.rs
@@ -1,11 +1,32 @@
 #[macro_use] extern crate nickel;
+extern crate hyper;
 
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 use nickel::{Nickel, HttpRouter};
+use nickel::status::StatusCode;
+use hyper::header::Location;
 
 fn main() {
     let mut server = Nickel::new();
-    
+    let short_urls = Arc::new(Mutex::new(HashMap::new()));
+    short_urls.lock().unwrap().insert("rust".to_string(), "https://rust-lang.org".to_string());
 
     server.get("/", middleware!("<h1>Nyanpassu!</h1>"));
+
+    let short_urls_clone = short_urls.clone();
+    server.get("/:shortkey", middleware! {|request, mut response|
+        let short_urls = short_urls_clone.lock().unwrap();
+        let shortkey = request.param("shortkey").unwrap();
+        if short_urls.contains_key(shortkey) {
+            let url = short_urls.get(shortkey).unwrap();
+            response.set(StatusCode::TemporaryRedirect);
+            response.set(Location(url.clone()));
+            return response.send("");
+        }
+        else {
+            return response.send("short url not found");
+        }
+    });
     server.listen("127.0.0.1:7000");
 }
