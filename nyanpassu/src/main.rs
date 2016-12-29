fd7f112a2bb0d75354397a200b81f255f6891c08
panpanick <claud@openmailbox.org>
2016-12-29 13:52:40 -0500

    Im missing something

--- nyanpassu/src/main.rs
+++ nyanpassu/src/main.rs
@@ -1,11 +1,15 @@
 #[macro_use] extern crate nickel;
 extern crate hyper;
+extern crate url;
 
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
+use sid::io::Read;
 use nickel::{Nickel, HttpRouter};
 use nickel::status::StatusCode;
 use hyper::header::Location;
+use hyper::Url;
+use url::form_urlencoded;
 
 fn main() {
     let mut server = Nickel::new();
@@ -17,6 +21,31 @@ fn main() {
         let mut data = HashMap::new();
         let short_urls = short_urls_clone.lock().unwrap();
         data.insert("url_count", short_urls.len().to_string());
+        return response.render("templates/index.tpl", &data);
+    });
+
+    let short_urls_clone = short_urls.clone();
+    server.post("/shorten", middleware!{|request, response|
+        let mut data = HashMap::new();
+        let short_urls = short_urls_clone.lock().unwrap();
+        data.insert("url_count", short_urls.len().to_string());
+
+        let mut post_data = String::new();
+        request.origin.read_to_string(&mut post_data).unwrap();
+        let form = parse_form(post_data.as_bytes());
+
+        let url = form.get("url").unwrap_or(&"".to_string()).to_string();
+        if url != "" {
+            if Url::parse(&url).is_ok() {
+                data.insert("result", "this is a good url".to_string());
+            }
+            else {
+                data.insert("result", "not a good url".to_string());
+            }
+        }
+        else {
+            data.insert("result", "enter a url".to_string()):
+        }
         return response.render("templates/index.tpl", &data);
     });
 
@@ -35,4 +64,13 @@ fn main() {
         }
     });
     server.listen("127.0.0.1:7000");
+}
+
+fn parse_form(form_data: &[u8]) -> HashMap<String, String> {
+    let mut hashmap = HashMap::new();
+    let parsed_form = form_urlencoded::parse(form_data);
+    for (key, value) in parsed_form {
+        hashmap.insert(key, value);
+    }
+    return hashmap;
 }
--- nyanpassu/templates/index.tpl
+++ nyanpassu/templates/index.tpl
@@ -6,6 +6,12 @@
 </head>
 <body>
   <h2>Nyanpa-su the URL shortener</h2>
+  <h3>{{result}}</h3>
+  <form action="/shorten" method="POST">
+    <input type="text" name="url" style="width: 35em;"/>
+    <input type="submit" value="nyanpassu!"/>
+  </form>
+  <br>
   <p>{{url_count}} urls shortened</p>
 </body>
 </html>
